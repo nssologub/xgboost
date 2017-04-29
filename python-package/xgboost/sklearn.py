329c0133b23bb18852a421528a292a46cbe12405
Nadzeya Salahub <nadzeya_salahub@epam.com>
2017-04-29 14:23:36 +0300

    Sklearn wrapper: added tree_method and grow_policy params support

--- python-package/xgboost/sklearn.py
+++ python-package/xgboost/sklearn.py
@@ -116,7 +116,7 @@ class XGBModel(XGBModelBase):
                  nthread=-1, gamma=0, min_child_weight=1, max_delta_step=0,
                  subsample=1, colsample_bytree=1, colsample_bylevel=1,
                  reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
-                 base_score=0.5, seed=0, missing=None):
+                 base_score=0.5, seed=0, missing=None, tree_method='auto', grow_policy='depthwise', max_leaves=255):
         if not SKLEARN_INSTALLED:
             raise XGBoostError('sklearn needs to be installed in order to use this module')
         self.max_depth = max_depth
@@ -139,6 +139,10 @@ class XGBModel(XGBModelBase):
         self.base_score = base_score
         self.seed = seed
         self.missing = missing if missing is not None else np.nan
+
+        self.tree_method = tree_method
+        self.grow_policy = grow_policy
+        self.max_leaves = max_leaves
         self._Booster = None
 
     def __setstate__(self, state):
@@ -360,14 +364,14 @@ class XGBClassifier(XGBModel, XGBClassifierBase):
                  nthread=-1, gamma=0, min_child_weight=1,
                  max_delta_step=0, subsample=1, colsample_bytree=1, colsample_bylevel=1,
                  reg_alpha=0, reg_lambda=1, scale_pos_weight=1,
-                 base_score=0.5, seed=0, missing=None):
+                 base_score=0.5, seed=0, missing=None, tree_method='auto', grow_policy='depthwise', max_leaves=255):
         super(XGBClassifier, self).__init__(max_depth, learning_rate,
                                             n_estimators, silent, objective,
                                             nthread, gamma, min_child_weight,
                                             max_delta_step, subsample,
                                             colsample_bytree, colsample_bylevel,
                                             reg_alpha, reg_lambda,
-                                            scale_pos_weight, base_score, seed, missing)
+                                            scale_pos_weight, base_score, seed, missing, tree_method, grow_policy, max_leaves)
 
     def fit(self, X, y, sample_weight=None, eval_set=None, eval_metric=None,
             early_stopping_rounds=None, verbose=True):
